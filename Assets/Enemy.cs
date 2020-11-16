e3ab7d89f264cacdc11ad46d1d7992273457885d
Linggar <linggar_asmara@apps.ipb.ac.id>
2020-11-16 10:55:52 +0700

    First Push

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    // Start is called before the first frame update
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
