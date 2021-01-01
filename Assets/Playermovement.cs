889320f58dc387569384261b7d973da2fe6c417b
Linggar <linggar_asmara@apps.ipb.ac.id>
2021-01-01 15:34:16 +0700

    Push 2

--- Assets/Playermovement.cs
+++ Assets/Playermovement.cs
@@ -13,7 +13,7 @@ public class Playermovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
---
+++ Assets/loadlevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadlevel : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if (collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if(useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else 
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+         }
+}
+}
